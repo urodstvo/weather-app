10b905e32b1cf184709bd36fef280977e0bd2c01
CLOWNAKIO <dead.kanji@mail.ru>
2024-03-16 12:38:30 +0300

    lint: fix eslint error

--- src/store/clientSettings.ts
+++ src/store/clientSettings.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 
 type State = {
     recentSearches: Array<{
